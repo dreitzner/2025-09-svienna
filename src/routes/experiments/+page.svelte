617fc255c42c0bb33a19f994bf6fa5806064e17b
Domenik Reitzner <domenik@reitzner.at>
2025-09-04 20:36:06 +0200

    First commit 🥇

---
+++ src/routes/experiments/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { wait } from "$lib/utils";
+
+    const title = $derived.by(async() => {
+        await wait(10000)
+        return 'Svelte'
+    })
+</script>
+
+<svelte:boundary>
+    Async {title}
+	{#snippet pending()}
+		<p>loading...</p>
+	{/snippet}
+</svelte:boundary>
---
+++ src/routes/slides/+page.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+    // TODO: slides here
+</script>
