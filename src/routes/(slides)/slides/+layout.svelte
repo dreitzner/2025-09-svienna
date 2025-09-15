e51398e769b438824a4a106023efb046d056e0ac
Domenik Reitzner <domenik@reitzner.at>
2025-09-15 20:42:22 +0200

    Sort stuff into folders 📂

---
+++ src/routes/(slides)/slides/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { LayoutProps } from "../../$types";
+    let { children }: LayoutProps = $props();
+</script>
+
+<main class="bg-gray-950 text-white min-h-screen w-screen">
+    {@render children()}
+</main>
